008862080f278575abe77fa8c23a31c7695d3e62
Kevin Bi <kevinbi@modular.com>
2025-04-28 11:04:51 -0600

    Dev branch

--- oh-my-zsh/custom/functions.zsh
+++ oh-my-zsh/custom/functions.zsh
@@ -7,11 +7,29 @@ add_to_path() {
 
 # Function wrapping the dotfiles custom dir copy over commands
 cp_zsh_custom() {
-  cp $DOTFILES/oh-my-zsh/custom/*(.) ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}
+  cp $DOTFILES/oh-my-zsh/custom/*(.) $HOME/.oh-my-zsh/custom
   if [[ -d $DOTFILES/oh-my-zsh/custom/plugins ]]; then
-    cp -r $DOTFILES/oh-my-zsh/custom/plugins/* ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom/plugins}
+    cp -r $DOTFILES/oh-my-zsh/custom/plugins/* $HOME/.oh-my-zsh/custom/plugins
   fi
   if [[ -d $DOTFILES/oh-my-zsh/custom/themes ]]; then
-    cp $DOTFILES/oh-my-zsh/custom/themes/* ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom/themes}
+    cp $DOTFILES/oh-my-zsh/custom/themes/* $HOME/.oh-my-zsh/custom/themes
   fi
+}
+
+# Function wrapping the dotfiles custom dir copy over commands
+cp_zsh_completions() {
+  if [[ ! -d $HOME/.oh-my-zsh/completions ]]; then
+    mkdir $HOME/.oh-my-zsh/completions
+  fi
+  cp $DOTFILES/oh-my-zsh/completions/*(.) $HOME/.oh-my-zsh/completions
+}
+
+# Get the argo password
+argo_passwd() {
+  k -n argocd get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d
+}
+
+# Easy way to login for AWS
+aws_plz() {
+  aws-sso-profile `aws-sso | fzf | awk -F ' *\\| *' '{print $7}'`
 }
